b8787d8c116bb66b00807b2dba4196e4ac09d57b
redmoon <bsef19a507@pucit.edu.pk>
2022-05-31 23:53:52 +0500

    MyListView class, Constractor added

--- StudentsInfo/app/src/main/java/com/example/studentsinfo/MyListView.java
+++ StudentsInfo/app/src/main/java/com/example/studentsinfo/MyListView.java
@@ -1,7 +1,15 @@
 package com.example.studentsinfo;
 
+import android.app.Activity;
 import android.widget.ArrayAdapter;
 
 public class MyListView extends ArrayAdapter {
+    private final Activity context;
+    private final String[] student_name;
+    private final String[] student_id;
+    private final String[] student_section;
+    private final String[] student_degree;
+    private final Integer[] imgid;
+
     
 }
