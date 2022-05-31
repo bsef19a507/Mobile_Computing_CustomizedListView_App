36684cab5084108eaf11d345889dce234bc5e26e
redmoon <bsef19a507@pucit.edu.pk>
2022-06-01 00:21:16 +0500

    MyViewAdapter constructor

--- StudentsInfo/app/src/main/java/com/example/studentsinfo/MyViewAdapter.java
+++ StudentsInfo/app/src/main/java/com/example/studentsinfo/MyViewAdapter.java
@@ -1,6 +1,17 @@
 package com.example.studentsinfo;
 
+import android.content.Context;
 import android.widget.ArrayAdapter;
 
+import androidx.annotation.NonNull;
+
+import java.util.ArrayList;
+
 public class MyViewAdapter extends ArrayAdapter<Student> {
+
+    public MyViewAdapter(@NonNull Context context, ArrayList<Student> studentArrayList) {
+        super(context, 0, studentArrayList);
+    }
 }
+
+
