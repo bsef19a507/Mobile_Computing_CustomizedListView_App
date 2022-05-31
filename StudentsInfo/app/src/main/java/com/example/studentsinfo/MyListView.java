8c42ef0aea58c5bf7083dc3b39176a5ecf806c88
redmoon <bsef19a507@pucit.edu.pk>
2022-06-01 00:09:57 +0500

    getView method implementation

--- StudentsInfo/app/src/main/java/com/example/studentsinfo/MyListView.java
+++ StudentsInfo/app/src/main/java/com/example/studentsinfo/MyListView.java
@@ -2,7 +2,12 @@ package com.example.studentsinfo;
 
 import android.app.Activity;
 import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
 import android.widget.ArrayAdapter;
+import android.widget.ImageView;
+import android.widget.TextView;
 
 import androidx.annotation.NonNull;
 
@@ -25,5 +30,25 @@ public class MyListView extends ArrayAdapter {
         this.student_degree = student_degree;
         this.imgid = imgid;
     }
-    sdfsa
+
+   
+    public View getView(int position, View view, ViewGroup parent) {
+        LayoutInflater inflater=context.getLayoutInflater();
+        View singleEntityView=inflater.inflate( R.layout.activity_main, null,true);
+        TextView name =  singleEntityView.findViewById (R.id.studentName);
+        ImageView image =  singleEntityView.findViewById (R.id.imageView);
+        TextView sID = singleEntityView.findViewById (R.id.studentID);
+        TextView section = singleEntityView.findViewById (R.id.studentSection);
+        TextView degree = singleEntityView.findViewById (R.id.studentDegree);
+
+
+        name.setText(student_name[position]);
+        sID.setText(student_id[position]);
+        section.setText(student_section[position]);
+        degree.setText(student_degree[position]);
+        image.setImageResource(imgid[position]);
+
+        return singleEntityView;
+    };
+
 }
