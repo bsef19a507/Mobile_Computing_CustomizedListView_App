833e1c38d5c4a961d0420874bb56d4ae2e3d00d3
redmoon <bsef19a507@pucit.edu.pk>
2022-06-02 00:41:47 +0500

    added data in arraylist to show students

--- StudentsInfo/app/src/main/java/com/example/studentsinfo/MainActivity.java
+++ StudentsInfo/app/src/main/java/com/example/studentsinfo/MainActivity.java
@@ -15,10 +15,10 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         ArrayList<Student> studentArrayList = new ArrayList<>();
-        studentArrayList.add(new Student("Ali", "001", "A","SE", 101));
-        studentArrayList.add(new Student("Saad", "002", "M", "IT",102));
-        studentArrayList.add(new Student("Aila", "003", "A", "CS",103));
-        studentArrayList.add(new Student("David", "004", "M","DS", 104));
+        studentArrayList.add(new Student("Ali", "001", "A","SE", R.drawable.a));
+        studentArrayList.add(new Student("Saad", "002", "M", "IT",R.drawable.b));
+        studentArrayList.add(new Student("Aila", "003", "A", "CS",R.drawable.c));
+        studentArrayList.add(new Student("David", "004", "M","DS", R.drawable.d));
         MyViewAdapter adapter = new MyViewAdapter(this, studentArrayList);
         ListView listView = findViewById(R.id.listView);
         listView.setAdapter(adapter);
